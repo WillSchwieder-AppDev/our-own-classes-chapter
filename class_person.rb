6303708a845e2ac472056a6fe8df69c2e196728f
WillSchwieder <wschwied@chicagobooth.edu>
2022-01-24 20:09:33 +0000

    Completing class portion

--- class_person.rb
+++ class_person.rb
@@ -6,6 +6,27 @@
 #  - An instance method, full_name, that puts first_name and last_name together
 #  - An instance method, age, that calculates the number of years between today and birthdate
 
+require "date"
+
+birthdate = "April 19, 1987"
+
+birthdate_d = Date.parse birthdate
+p birthdate_d.year
+
+
+
 class Person
-  
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :birthdate
+
+  def full_name
+    return self.first_name + " " + self.last_name
+  end
+
+  def age
+    birthdate_d = Date.parse self.birthdate
+    return Date.today.year - birthdate_d.year
+  end
+
 end
